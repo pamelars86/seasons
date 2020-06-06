e682d0c9d412005d0b3274c13935cb97af6c4119
pamelars86 <pamela.rosales@pucp.pe>
2020-06-06 13:57:06 -0300

    sending files to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Season from './Season';
+import Loading from './Loading';
+import useLocation from './location';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  
+  const [lat, errorMessage] = useLocation();
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error : {errorMessage}</div>
+  } else if (lat){
+    content = <Season lat={lat} />;
+  } else {
+     content = <Loading message="Please accept location request" />;
+  }
+
+  return <div className="border red">{content}</div>
 }
 
 export default App;
